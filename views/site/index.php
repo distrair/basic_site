8e336741e41c560cdc707dd9d3e2153a0929e613
distrair <78724291+distrair@users.noreply.github.com>
2023-03-10 12:32:57 +0300

    🍮🎄  Ⓙ  💘🌷

--- views/site/index.php
+++ views/site/index.php
@@ -5,5 +5,6 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
+	<div class="text-center display-4 mt-3">Создать портфолио</div>
+	<div class="text-center display-5">Единственный сайт для создания портфолио, многофункиональный и простой</div>
 </div>
