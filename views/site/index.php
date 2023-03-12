3c762087527f99a245dd5dead622537e3e5dfc5c
distrair <116867826+damridk@users.noreply.github.com>
2023-03-12 20:52:28 +0300

    idk i did something to automatically login and redirect to a page to create a portfolio

--- views/site/index.php
+++ views/site/index.php
@@ -1,10 +1,20 @@
 <?php
-
+use yii\helpers\Url;
+use yii\helpers\Html;
 /** @var yii\web\View $this */
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 	<div class="text-center display-4 mt-3">Создать портфолио</div>
-	<div class="text-center display-5">Единственный сайт для создания портфолио, многофункиональный и простой</div>
+	<div class="text-center display-5 mt-3">Единственный сайт для создания портфолио, многофункиональный и простой</div>
+    <?php if (Yii::$app->user->isGuest) { ?>
+    <div class="col text-center mt-3">
+        <?= Html::a('Зарегистрироваться', ['/user/create'], ['class' => 'btn btn-success']) ?>
+    </div>
+    <?php } else {?>
+    <div class="col text-center mt-3">
+        <?= Html::a('Создать портфолио', ['/portfolio/create'], ['class' => 'btn btn-success']) ?>
+    </div>
+    <?php } ?>
 </div>
